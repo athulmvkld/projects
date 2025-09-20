4731e47c9d77464cdd33743f7cc02694cec71e1d
Athul M V <athulmvkoyilandy@gmail.com>
2025-09-20 23:23:06 +0530

    dr and booking files

--- doctor/home/views.py
+++ doctor/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Departments
+from .models import Departments,Doctors
+from .forms import BookingForm
 # Create your views here.
 def index(request):
     person={
@@ -14,10 +15,22 @@ def about(request):
     return render(request,'about.html')
 
 def booking(request):
-    return render(request,'booking.html')
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'confirmation.html')
+    form = BookingForm()
+    dict_form={
+        'form': form
+    }
+    return render(request, 'booking.html', dict_form)
 
 def doctors(request):
-    return render(request,'doctors.html')
+    dict_docs = {
+        'doctors': Doctors.objects.all()
+    }
+    return render(request, 'doctors.html', dict_docs)
 
 def contact(request):
     return render(request,'contact.html')
